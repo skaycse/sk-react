eb6234f221cb4415e806927fad53d6abc1b26356
Sreekumar <skaycse@gmail.com>
2022-03-27 18:09:38 +0530

    login issue fix

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,30 +1,31 @@
-import React from "react";
-import { Navigate } from 'react-router';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { deleteCookie, getCookie } from "../../utils/cookie"
 
 export const Dashboard = () => {
+    const navigate = useNavigate();
     const clearMe = () => {
         deleteCookie('access_token');
+        navigate('/');
     }
     const userAccessToken = getCookie('access_token');
 
     const checkState = () => {
         if (!userAccessToken) {
-            return <Navigate to='/' />;
+            navigate('/');
         }
     }
 
-
-    // useEffect(() => {
-    //     checkState();
-    // });
+    useEffect(() => {
+        checkState();
+    }, []);
 
 
     return (
 
         <div>
             <h1>Hello World!!</h1>
-            <button onClick={clearMe()} >Logout</button>
+            <button onClick={() => clearMe()} >Logout</button>
         </div>
 
     )
